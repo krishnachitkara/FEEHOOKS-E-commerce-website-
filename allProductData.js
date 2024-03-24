7f63faf5524894ac28e39ae9dd36954e52e3886e
krishna <krishna520.be22@chitkara.edu.in>
2024-03-24 13:52:36 +0530

    First Commit

---
+++ allProductData.js
@@ -0,0 +1,291 @@
+var allProductData = [{
+    imgURL: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/19e47768-0630-4653-ac0e-4e802578f316/mercurial-superfly-9-club-multi-ground-high-top-football-boot-PwHVpF.png",
+    price: "8999.00",
+    name: "Nike Mercurial Superfly 9 Elite",
+    type: "Men lifestyle",
+    gender: "Male"
+},
+{
+    imgURL: "https://assets.adidas.com/images/w_383,h_383,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/afa8f5a7d93a42bbaea1ada9009e3f2a_9366/future-icons-3-bar-pants.jpg",
+    price: "2519.50",
+    name: "FUTURE ICONS 3-BAR PANTS",
+    type: "Men Training",
+    gender: "Male"
+},
+{
+    imgURL: "https://assets.adidas.com/images/w_383,h_383,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/b58b8708db314a4db3c6ad8500a5bda1_9366/adi-bounce-shoes.jpg",
+    price: "1900.00",
+    name: "Adi-Bounce Shoes",
+    type: "Men Running",
+    gender: "Male"
+},
+{
+    imgURL: "https://assets.adidas.com/images/w_276,h_276,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/0b5ba76a98804ec4ba20ad13011f2c8d_9366/Deco_Trefoil_Tee_Burgundy_H31333_21_model.jpg",
+    price: "2499.00",
+    name: "Deco Trefoil Tee",
+    type: "Men Lifestyle",
+    gender: "Male"
+},
+{
+    imgURL: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/ebd4e488-3422-4ce9-8707-212869ae5f12/jordan-flight-mvp-85-t-shirt-hdS3DZ.png",
+    price: "999.00",
+    name: "Jordan Flight MVP 85",
+    type: "Men Lifestyle",
+    gender: "Male"
+},
+{
+    imgURL: "https://assets.adidas.com/images/w_276,h_276,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/9e4850c6019841138e38a7fa00de8937_9366/Adilette_Slides_Black_280647_01_standard.jpg",
+    price: "3599.00",
+    name: "Adilette Slides",
+    type: "Men originals",
+    gender: "Male"
+},
+{
+    imgURL: "https://assets.adidas.com/images/w_276,h_276,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/13085062e6fd497fb6faac5e00302b1b_9366/Forum_Mid_Shoes_White_FY4975_01_standard.jpg",
+    price: "9999.00",
+    name: "Forum Mid Shoes",
+    type: "Men originals",
+    gender: "Male"
+},
+{
+    imgURL: "https://assets.adidas.com/images/w_383,h_383,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/4c20e6c841a442e18870ad2400375df4_9366/graphics-camo-sweat-pants.jpg",
+    price: "4199.50",
+    name: "Graphics Camo Sweat Pants",
+    type: "women Lifestyle",
+    gender: "Female"
+},
+{
+    imgURL: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/0fe6b6fd-1e73-4300-92b8-6537a8514ac1/sb-skate-jumper-R2xZ3c.png",
+    price: "2999.00",
+    name: "Adicolor Classics Corded Velour Loose Tee",
+    type: "Women originals",
+    gender: "Female"
+},
+{
+    imgURL: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/7fd130fd-dfbe-4114-bccb-faa6fc32207c/jordan-renegade-jacket-qkkLzr.png",
+    price: "4999.00",
+    name: "Nike SB",
+    type: "Women Lifestyle",
+    gender: "Female"
+},
+{
+    imgURL: "https://assets.adidas.com/images/w_276,h_276,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/8ec15ae5654f42da932cad1900d4bb55_9366/Tripleplatforum_Disney_Shoes_Blue_GZ7005_01_standard.jpg",
+    price: "12999.00",
+    name: "Tripleplatforum Disney Shoes",
+    type: "Women Lifestyle",
+    gender: "Female"
+},
+{
+    imgURL: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/069d12f5-bffd-4aef-b20a-8b3130045425/sportswear-essential-woven-jacket-SGzs3v.png",
+    price: "11999.00",
+    name: "Suit Jacket",
+    type: "Women originals",
+    gender: "Female"
+},
+{
+    imgURL: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/2b32acb3-3cac-4f99-a33a-38b5369de3e6/sportswear-essential-woven-high-waisted-trousers-BWGbCZ.png",
+    price: "2799.00",
+    name: "Women's Woven High-Waisted Trousers",
+    type: "Women originals",
+    gender: "Female"
+},
+{
+    imgURL: "https://assets.adidas.com/images/w_276,h_276,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/078da03558b54ce19e14ad240022e710_9366/Backpack_Green_H09127_01_standard.jpg",
+    price: "5999.00",
+    name: "Backpack",
+    type: "Women Lifestyle",
+    gender: "Female"
+},
+{
+    imgURL: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/f103b162-1fcd-4e33-8fe9-9d47084e236c/jordan-brand-t-shirt-wx6hd6.png",
+    price: "2299.00",
+    name: "Own the Run Tee",
+    type: "Performance",
+    gender: "Female"
+},
+{
+    imgURL: "https://assets.adidas.com/images/w_383,h_383,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/4c20e6c841a442e18870ad2400375df4_9366/graphics-camo-sweat-pants.jpg",
+    price: "3599.00",
+    name: "Tights with Velvet Stripes and Trefoil Rivet",
+    type: "Originals",
+    gender: "Female"
+},
+{
+    imgURL: "https://assets.adidas.com/images/w_276,h_276,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/333f2ab0836f42aa9fa0ad22012a5077_9366/H28826_01_laydown.jpg",
+    price: "1399.50",
+    name: "Disney Mickey Mouse Teet",
+    type: "Sportswear",
+    gender: "kids"
+},
+{
+    imgURL: "https://assets.adidas.com/images/w_276,h_276,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/692dca8d91cc48098783acc9016fbcb3_9366/adidas_ZX_8000_x_LEGO(r)_Shoes_Black_GZ8216_01_standard.jpg",
+    price: "4000.00",
+    name: "adidas ZX 8000 x LEGO",
+    type: "Kids Lifestyle",
+    gender: "kids"
+},
+{
+    imgURL: "https://assets.adidas.com/images/w_276,h_276,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/baf1dec5b4f44d27896dace100ae5d7c_9366/GT9484_01_laydown.jpg",
+    price: "3599.00",
+    name: "Disney Comfy Princesses Pants",
+    type: "Sportswear",
+    gender: "kids"
+},
+{
+    imgURL: "https://assets.adidas.com/images/w_276,h_276,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/88ec36d92a5840f49c51ab1a01113414_9366/EI9897_01_laydown.jpg",
+    price: "1400.00",
+    name: "Juventus Home Shorts",
+    type: "Performance",
+    gender: "kids"
+},
+{
+    imgURL: "https://assets.adidas.com/images/w_276,h_276,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/06a1bc30cca84cbdbf07ad19010fcfd7_9366/H16387_00_plp_standard.jpg",
+    price: "2099.50",
+    name: "Marvel Spider-Man Primegreen Backpacks",
+    type: "Performance",
+    gender: "kids"
+},
+{
+    imgURL: "https://assets.adidas.com/images/w_276,h_276,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/450b535929674340a0a5ad120115b838_9366/GU8814_00_plp_standard.jpg",
+    price: "2099.50",
+    name: "Disney Princesses Primegreen Backpack",
+    type: "Performance",
+    gender: "kids"
+},
+{
+    imgURL: "https://assets.adidas.com/images/w_276,h_276,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/526c5eda6cdb429a8a45ad2400c382e0_9366/adidas_x_LEGO(r)_Sport_Shoes_Yellow_FZ5439_01_standard.jpg",
+    price: "6599.00",
+    name: "adidas x LEGO",
+    type: "Kids Running",
+    gender: "kids"
+},
+{
+    imgURL: "https://assets.adidas.com/images/w_276,h_276,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/d8b19f5114d24d878e72ad2600f615cd_9366/adidas_Forta_Run_x_LEGO(r)_VIDIYOtm_Shoes_Black_G57947_01_standard.jpg",
+    price: "6599.00",
+    name: "adidas Forta Run",
+    type: "Kids Running",
+    gender: "kids"
+}
+];
+
+var wishlist = JSON.parse(localStorage.getItem("wishlistdb")) || []
+var len = wishlist.length;
+var cartlist = JSON.parse(localStorage.getItem("cartlistdb")) || []
+var le = cartlist.length;
+
+displayData(allProductData);
+document.querySelector("#wishlistLen").textContent = len;
+document.querySelector("#cartLen").textContent = le;
+
+function displayData(arr) {
+    arr.map(function (element, index) {
+        var div = document.createElement("div");
+
+        var img = document.createElement("img");
+        img.setAttribute("src", element.imgURL);
+
+        var price = document.createElement("p");
+        price.textContent = "Rs. " + element.price;
+        price.setAttribute("class", "price");
+
+        var symDiv = document.createElement("div");
+        symDiv.innerHTML = '<i class="far fa-heart ">';
+        symDiv.setAttribute("class", "wishlistSymbol");
+        var count = 1
+
+        symDiv.addEventListener("click", symbalcolor)
+        function symbalcolor() {
+            count++
+            if (count % 2 == 0) {
+                console.log("solid");
+                symDiv.innerHTML = '<i class="fas fa-heart"></i>';
+                wishlist.push(element);
+                localStorage.setItem('wishlistdb', JSON.stringify(wishlist));
+                displaybestOfAdidasData(bestOfAdidas);
+
+            }
+            else {
+                console.log("dull");
+                symDiv.innerHTML = '<i class="far fa-heart"></i>';
+                symDiv.addEventListener("click", function () {
+                    deleteRow(index, count)
+                })
+
+            }
+
+        }
+
+        var cartSymDiv = document.createElement("div");
+        cartSymDiv.innerHTML = '<i class="fas fa-cart-plus"></i>';
+        cartSymDiv.setAttribute("class", "cartSym");
+        cartSymDiv.addEventListener("click", function () {
+            addtocart(element, cartSymDiv);
+        })
+
+        var name = document.createElement("p");
+        name.textContent = element.name;
+
+        var type = document.createElement("p");
+        type.textContent = element.type;
+
+        div.append(img, price, symDiv, cartSymDiv, name, type);
+        document.querySelector("#allProductDiv").append(div);
+    });
+}
+
+function displaysorted() {
+    document.querySelector("#allProductDiv").innerHTML = "";
+    var sortValue = document.querySelector("#sortByPrice").value;
+    var sortedarr = allProductData;
+    if (sortValue == "lowToHigh") {
+        sortedarr = sortedarr.sort(function (a, b) {
+            return Number(a.price) - Number(b.price);
+        });
+        displayData(sortedarr);
+    }
+    else if (sortValue == "highToLow") {
+        sortedarr = sortedarr.sort(function (a, b) {
+            return Number(b.price) - Number(a.price);
+        });
+        displayData(sortedarr);
+    }
+    else if(sortValue == "") {
+        displayData(allProductData);
+    }
+}
+function addtocart(element, cartSymDiv) {
+    cartSymDiv.innerHTML = '<i class="fas fa-shopping-bag"></i>';
+    cartlist.push(element);
+    localStorage.setItem('cartlistdb', JSON.stringify(cartlist));
+
+}
+
+function sortByGender(){
+    document.querySelector("#allProductDiv").innerHTML = "";
+    var sortValue = document.querySelector("#sortByGender").value;
+    
+    if(sortValue == "male"){
+        var sortedarr = allProductData;
+        sortedarr = sortedarr.filter(function(element){
+            return (element.gender == "Male");
+        });
+        displayData(sortedarr);
+    }
+    else if(sortValue == "female"){
+        var sortedarr = allProductData;
+        sortedarr = sortedarr.filter(function(element){
+            return (element.gender == "Female");
+        });
+        displayData(sortedarr);
+    }
+    else if(sortValue == "kids"){
+        var sortedarr = allProductData;
+        sortedarr = sortedarr.filter(function(element){
+            return (element.gender == "kids");
+        });
+        displayData(sortedarr);
+    }
+    else if(sortValue == ""){
+        displayData(allProductData);
+    }
+}
